329abd001b2f8bee0b7805bdd25384677c38e8b1
გიორგი <igroig@gmail.com>
2026-06-06 08:45:40 +0400

    Add star symbol to parsed text output

--- bulk-parser.js
+++ bulk-parser.js
@@ -114,7 +114,7 @@ function parseBulkDSL(raw) {
     if (atmM) {
       flush();
       speaker = null;
-      cur.text += (cur.text ? '<br>' : '') + _esc(atmM[1].trim());
+      cur.text += (cur.text ? '<br>' : '') + '✦ ' + _esc(atmM[1].trim());
       continue;
     }
 
